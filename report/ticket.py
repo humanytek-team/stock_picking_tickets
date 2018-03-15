e8c5035365091814791d99e43fee10ecf85b90b3
Moisés Alejandro Navarro Presas <moisalejandro@gmail.com>
2018-03-15 16:58:05 -0600

    [BUG] IVA on returns

--- report/ticket.py
+++ report/ticket.py
@@ -28,6 +28,8 @@ class TicketReport(models.AbstractModel):
             'get_total': self.get_total,
             'get_price_by_sale': self.get_price_by_sale,
             'get_price_by_purchase': self.get_price_by_purchase,
+            'get_sale_by_name': self.get_sale_by_name,
+            'get_purchase_by_name': self.get_purchase_by_name,
             'amount_to_text': self.amount_to_text,
         }
 
@@ -35,16 +37,28 @@ class TicketReport(models.AbstractModel):
             'stock_picking_tickets.template_inv_ticket', docargs)
 
     @api.model
+    def get_sale_by_name(self, name):
+        return self.env['sale.order'].search([('name', '=', name)], limit=1)
+
+    @api.model
+    def get_purchase_by_name(self, name):
+        return self.env['purchase.order'].search([('name', '=', name)], limit=1)
+
+    @api.model
+    def get_sale_line(self, sale, product):
+        return self.env['sale.order.line'].search([('order_id', '=', sale.id), ('product_id', '=', product.id)], limit=1)
+
+    @api.model
+    def get_purchase_line(self, purchase, product):
+        return self.env['purchase.order.line'].search([('order_id', '=', purchase.id), ('product_id', '=', product.id)], limit=1)
+
+    @api.model
     def get_price_by_sale(self, sale, product):
-        sale_id = self.env['sale.order'].search(
-            [('name', '=', sale)], limit=1).id
-        return self.env['sale.order.line'].search([('order_id', '=', sale_id), ('product_id', '=', product.id)]).price_unit
+        return self.get_sale_line(sale, product).price_unit
 
     @api.model
     def get_price_by_purchase(self, purchase, product):
-        purchase_id = self.env['purchase.order'].search(
-            [('name', '=', purchase)], limit=1).id
-        return self.env['purchase.order.line'].search([('order_id', '=', purchase_id), ('product_id', '=', product.id)]).price_unit
+        return self.get_purchase_line(purchase, product).price_unit
 
     @api.model
     def get_total_line(self, product_qty, product_price, product_taxes=False):
@@ -59,19 +73,47 @@ class TicketReport(models.AbstractModel):
         return total
 
     @api.model
+    def get_total_return(self, picking, taxes=False, tax_total=False):
+        total = 0
+        total_tax = 0
+        if picking.picking_type_code == 'outgoing':
+            purchase = self.get_purchase_by_name(picking.group_id.name)
+            for move in picking.move_lines:
+                if move.state == 'done' and move.product_uom_qty > 0:
+                    total_line = move.product_uom_qty * \
+                        self.get_price_by_purchase(purchase, move.product_id)
+                    total += total_line
+                    if taxes:
+                        for tax in self.get_purchase_line(purchase, move.product_id).taxes_id:
+                            total += total_line * tax.amount / 100
+                            if tax_total:
+                                total_tax += total_line * tax.amount / 100
+        elif picking.picking_type_code == 'incoming':
+            sale = self.get_sale_by_name(picking.group_id.name)
+            for move in picking.move_lines:
+                if move.state == 'done' and move.product_uom_qty > 0:
+                    total_line = move.product_uom_qty * \
+                        self.get_price_by_sale(sale, move.product_id)
+                    total += total_line
+                    if taxes:
+                        for tax in self.get_sale_line(sale, move.product_id).tax_id:
+                            total += total_line * tax.amount / 100
+                            if tax_total:
+                                total_tax += total_line * tax.amount / 100
+        if tax_total:
+            return total_tax
+        return total
+
+    @api.model
     def get_total(self, picking, taxes=False, tax_total=False):
+        if picking.group_id:
+            return self.get_total_return(picking, taxes, tax_total)
         total = 0
         total_tax = 0
         if picking.picking_type_code == 'outgoing':
             for move in picking.move_lines:
                 if move.state == 'done' and move.product_uom_qty > 0:
-                    if picking.group_id:
-                        total_line = move.product_uom_qty * \
-                            self.get_price_by_purchase(
-                                picking.group_id.name, move.product_id)
-                    else:
-                        total_line = move.product_uom_qty * \
-                            move.procurement_id.sale_line_id.price_unit
+                    total_line = move.product_uom_qty * move.procurement_id.sale_line_id.price_unit
                     total += total_line
                     if taxes:
                         for tax in move.procurement_id.sale_line_id.tax_id:
@@ -81,13 +123,7 @@ class TicketReport(models.AbstractModel):
         elif picking.picking_type_code == 'incoming':
             for move in picking.move_lines:
                 if move.state == 'done' and move.product_uom_qty > 0:
-                    if picking.group_id:
-                        total_line = move.product_uom_qty * \
-                            self.get_price_by_sale(
-                                picking.group_id.name, move.product_id)
-                    else:
-                        total_line = move.product_uom_qty * \
-                            move.purchase_line_id.price_unit
+                    total_line = move.product_uom_qty * move.purchase_line_id.price_unit
                     total += total_line
                     if taxes:
                         for tax in move.purchase_line_id.taxes_id:
